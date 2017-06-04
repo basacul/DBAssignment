0dbf45afedc422ec17dad7b49882d69a7dda3cd4
basacul <antelo.lucas@outlook.com>
2017-06-04 23:25:12 +0200

    finally done!

--- java/DataSanitizer.java
+++ java/DataSanitizer.java
@@ -17,7 +17,15 @@ public class DataSanitizer {
 	private static final String FILESYSTEM_SEPARATOR = FileSystems.getDefault().getSeparator();
 	private static final String OUTPUT_FILE_PREFIX = "sanitized_";
 	private static CSVParser csvParser;
+	private static int i = 0;
+	private static int hashtagID = 0;
+	private static int linkID = 0;
 
+	/*tested the number of errors thrown along the population of the database election
+	* which was used to test and count the number of erroneous entries along the
+	* sanitization process
+	*/
+	private static int error = 0;
 
 	public static void main(String[] args) throws Exception {
 		// Input/Output files
@@ -31,23 +39,14 @@ public class DataSanitizer {
 
 		//for jdbc
 		Connection c = null;
-		Statement s = null;
-
-		//for retrieving the hashtags
-		String plainText;
-		ArrayList<String> hashtags = new ArrayList<String>();
-
-		ArrayList<String> links = new ArrayList<String>();
-
-		//for the id in Tweet and much easier to compute and retrieve
-		int i = 0;
+	  Statement s = null;
 
 		try{
 			Class.forName("org.postgresql.Driver");
 
 			c = DriverManager.getConnection(
 				"jdbc:postgresql://[::1]:5432/Election",
- 				"postgres", "postgres");
+ 				"admin", "admin");
 
 				s = c.createStatement();
 
@@ -72,87 +71,8 @@ public class DataSanitizer {
 				continue;
 			}
 
-
-			plainText = line.get(1);
-
-			//hashtag retrieved go to next iteration
-			boolean breakLoop = false;
-
-			//retrieve the hash and dynamically update length condition in outer for loop
-			for(int w = 0; w < plainText.length(); w++){
-				if(plainText.charAt(w) == '#'){
-					for(int e = w; e < plainText.length() && !breakLoop; e++){
-						if(plainText.charAt(e) == ' ' || plainText.charAt(e) == ','){
-							hashtags.add(plainText.substring(w, e));
-							plainText = plainText.substring(0,w) + plainText.substring(e+1, plainText.length() - 1);
-							breakLoop = true;
-						}
-					}
-				}
-				breakLoop = false;
-			}
-
-
-			//link retrieved go to next iteration
-			breakLoop = false;
-			int diff = 0;
-
-			//retrieve the links and dynamically update length condition in outer for loop
-			for(int w = 0; w < plainText.length(); w++){
-				diff = w + 4;
-				if(diff < plainText.length() &&
-					plainText.charAt(w) == 'h' &&
-					plainText.charAt(w+1) == 't' &&
-					plainText.charAt(w+2) == 't' &&
-					plainText.charAt(w+3) == 'p'){
-					for(int e = w; e < plainText.length() && !breakLoop; e++){
-						if(plainText.charAt(e) == ' ' || plainText.charAt(e) == ','){
-							links.add(plainText.substring(w, e));
-							//System.out.println(plainText.substring(w, e));
-							plainText = plainText.substring(0,w) + plainText.substring(e, plainText.length() - 1);
-							breakLoop = true;
-						}
-					}
-				}
-				breakLoop = false;
-			}
-
-			/*for(int z = 0; z < links.size(); z++){
-				System.out.println(links.get(z));
-			}*/
-			//replaces the T in the time string with an empty space " "
-			line.set(4, (line.get(4).substring(0,10) + " " + line.get(4).substring(11, line.get(4).length() - 1)));
-
-			//populate the tables in the database Election
-			try{
-
-    			s.executeUpdate("INSERT INTO Tweet (timestamp, favouriteCount, sourceUrl, quoteStatus, truncated, retweetCount, handle) VALUES ('" + line.get(4) + "', '" + line.get(8) + "', '" + line.get(9) + "', " + line.get(6) + ", " + line.get(10) + ", " + line.get(7) + ", '" + line.get(0) + "')");
-					s.executeUpdate("INSERT INTO Content (Tweet_id)" + "VALUES ('" + i + "')" );
-    			s.executeUpdate("INSERT INTO Text (Content_id, text) " + "VALUES ('" + i + "', '" + plainText + "')");
-
-					for(int q = 0; q < hashtags.size(); q++){
-						s.executeUpdate("INSERT INTO Hashtag (Content_id, hashtag) VALUES ('" + i + "' , '" + hashtags.get(q) + "')");
-					}
-
-					for(int q = 0; q < links.size(); q++){
-						s.executeUpdate("INSERT INTO Link (Content_id, link) " + "VALUES ('" + i + "', '" + links.get(q) + "')");
-					}
-
-					s.executeUpdate("INSERT INTO TargetHandle (Content_id, targetHandle) VALUES ('"  + i + "', '" + line.get(0) + "')");
-    			s.executeUpdate("INSERT INTO Retweet (id, Tweet_id, originalHandle) " + "VALUES (" + i + ", SELECT id FROM Tweet WHERE id=" + i + ", '" + line.get(3) + "')");
-    			s.executeUpdate("INSERT INTO Reply (Tweet_id, replyHandle) " + "VALUES ( SELECT id FROM Tweet WHERE id=" + i + ", '" + line.get(5) + "')");
-
-			}catch(Exception e){
-				//System.out.println("Still unlucky?" + e.getMessage());
-			}
-
-			//increments the id, which will be set automatically for each Tweet entry
-			//but i is used to decrease computation through jdbc
-			++i;
-
-			//clear arraylist hashtags
-			hashtags.clear();
-			links.clear();
+			//populates the tables in the database election with the entries in the actual line
+			populateDB(line, s);
 
 			// Append "("
 			line.set(0, "(" + line.get(0));
@@ -180,21 +100,117 @@ public class DataSanitizer {
 			System.out.println(e.getMessage());
 			System.out.println("Failure");
 		}
-
+		System.out.println("Number of lines corrupted lines : " + error );
 		// Close I/O
 		scanner.close();
 		buffWriter.close();
 		fileWriter.close();
 	}
 
+	private static void populateDB(List<String> line, Statement s){
+		//for retrieving the hashtags and links
+		String plainText = line.get(1);
+		ArrayList<String> hashtags = new ArrayList<String>();
+		ArrayList<String> links = new ArrayList<String>();
+
+		//for the id in Tweet and much easier to compute and retrieve
+
+
+		//hashtag retrieved go to next iteration
+		boolean breakLoop = false;
+
+
+		for(int c = 0;c < plainText.length();c++){
+			if(plainText.charAt(c) == '\''){
+				plainText = plainText.substring(0,c)+' '+plainText.substring(c+1);
+			}
+		}
+		//retrieve the hash and dynamically update length condition in outer for loop
+		for(int w = 0; w < plainText.length(); w++){
+			if(plainText.charAt(w) == '#'){
+				for(int e = w; e < plainText.length() && !breakLoop; e++){
+					if(plainText.charAt(e) == ' ' || plainText.charAt(e) == ','  || plainText.charAt(e) == '\'' || e +1 == plainText.length()){
+						// System.out.println("hashtags");
+						hashtags.add(plainText.substring(w, e));
+						w = e;
+						breakLoop = true;
+					}
+				}
+			}
+			breakLoop = false;
+		}
+
+
+		//link retrieved go to next iteration
+		breakLoop = false;
+		int diff = 0;
+
+		//retrieve the links and dynamically update length condition in outer for loop
+		for(int w = 0; w < plainText.length(); w++){
+			diff = w + 3;
+			if(diff < plainText.length() && plainText.charAt(w) == 'h' &&
+				plainText.charAt(w+1) == 't' &&
+				plainText.charAt(w+2) == 't' &&
+				plainText.charAt(w+3) == 'p'){
+				for(int e = w; e < plainText.length() && !breakLoop; e++){
+					// System.out.println("links");
+					if(plainText.charAt(e) == ' ' || plainText.charAt(e) == ','  || plainText.charAt(e) == '\'' || e +1 == plainText.length()){
+						links.add(plainText.substring(w, e));
+						// System.out.println(plainText.substring(w, e));
+						w = e;
+						breakLoop = true;
+					}
+				}
+			}
+			breakLoop = false;
+		}
+		line.set(4, (line.get(4).substring(0,10) + " " + line.get(4).substring(11, line.get(4).length() - 1)));
+		//System.out.println(line.get(4));
+		//populate the tables in the database Election
+		try{
+			// try{
+			// 	 s.executeUpdate("INSERT INTO Text (id, idContent, plaintext) " + "VALUES ('" + i + "','" + i + "', '" + plainText + "')");
+
+			// 	 }catch(Exception e){
+			// 	 		System.out.println("What?");
+			// 	  			return;
+			// 	  		}
+  			s.executeUpdate("INSERT INTO Tweet (id, datetime, favouriteCount, sourceUrl, quoteStatus, truncated, retweetCount, handle) VALUES ('" + i + "','" + line.get(4) + "', '" + line.get(8) + "', '" + line.get(9) + "', " + line.get(6) + ", " + line.get(10) + ", " + line.get(7) + ", '" + line.get(0) + "')");
+			s.executeUpdate("INSERT INTO Content (id, idTweet)" + "VALUES ('" + i + "', '" + i + "')" );
+  			s.executeUpdate("INSERT INTO Text (id, idContent, plaintext) " + "VALUES ('" + i + "','" + i + "', '" + plainText + "')");
+
+			for(int q = 0; q < hashtags.size(); q++){
+				s.executeUpdate("INSERT INTO Hashtag (id, idContent, hashtag) VALUES ('" + (hashtagID++) + "' , '" + i + "' , '" + hashtags.get(q) + "')");
+			}
+
+			for(int q = 0; q < links.size(); q++){
+				s.executeUpdate("INSERT INTO Link (id, idContent, link) " + "VALUES ('" + (linkID++) + "' , '" + i + "', '" + links.get(q) + "')");
+			}
+
+			s.executeUpdate("INSERT INTO TargetHandle (id, idContent, targetHandle) VALUES ('"  + i + "','"  + i + "', '" + line.get(0) + "')");
+  			s.executeUpdate("INSERT INTO Retweet (id, idTweet, originalHandle) " + "VALUES ( '"  + i + "','" + i + "', '" + line.get(3) + "')");
+  			s.executeUpdate("INSERT INTO Reply (idTweet, replyHandle) " + "VALUES ('" + i + "', '" + line.get(5) + "')");
+
+		}catch(Exception e){
+			System.out.println("Still unlucky?" + e.getMessage());
+			++error;
+		}
+
+		//increments the id, which will be set automatically for each Tweet entry
+		//but i is used to decrease computation through jdbc
+		++i;
+
+		//clear arraylist hashtags
+		hashtags.clear();
+		links.clear();
+	}
+
 	private static boolean verifyLine(List<String> line) {
 		boolean lineIsCorrect = true;
-
 		// Throw away all lines not containing exactly CSVParserImpl.DEFAULT_SEPARATOR_MAX amount of separators and an even amount of "
 		if (line.size() != 11 || !lineTextIsBalanced(line)) {
 			lineIsCorrect = false;
 		}
-
 		return lineIsCorrect;
 	}
 
@@ -211,7 +227,6 @@ public class DataSanitizer {
 		if ((stack.size() % 2) != 0) {
 			isBalanced = false;
 		}
-
 		return isBalanced;
 	}
 
